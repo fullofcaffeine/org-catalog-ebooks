00cd7e427b4fd687751cfd7907f69ac49ca4b17f
Marcelo de Moraes Serpa <fullofcaffeine@gmail.com>
2012-09-02 22:45:20 -0500

    This is not needed

--- catalog.el
+++ catalog.el
@@ -1,15 +1,9 @@
-
-
-
-(relisp-start-slave "/Users/fullofcaffeine/workspace/code/org-catalog-files/relisp_slave_server.rb")
+(relisp-start-slave "/Users/fullofcaffeine/workspace/code/org-catalog-ebooks/relisp_slave_server.rb")
 
 (defun catalog-ebooks () (interactive)
-        (setenv "LANG" "en_US.UTF-8")
-        (setenv "LC_ALL" "en_US.UTF-8")
-        (setenv "LC_CTYPE" "en_US.UTF-8")
 ;        (shell-command-to-string  "ruby
 ;        /Users/fullofcaffeine/workspace/code/org-catalog-files/catalog.rb"))
-        (setq ebooks (shell-command-to-string "/Users/fullofcaffeine/.rvm/bin/rvm ruby-1.9.3-p194 do ruby /Users/fullofcaffeine/workspace/code/org-catalog-files/catalog.rb"))
+        (setq ebooks (shell-command-to-string "/Users/fullofcaffeine/.rvm/bin/rvm ruby-1.9.3-p194 do ruby /Users/fullofcaffeine/workspace/code/org-catalog-ebooks/catalog.rb"))
 
  (let ((result))
       (dolist (el (ruby-eval ebooks))
